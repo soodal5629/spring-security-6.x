5713ea7649597def9ea77843138da116ddd64677
jcl <chaelynjang@lotte.net>
2024-07-17 16:32:43 +0900

    멀티 세션 관리 확인

--- src/main/java/io/security/springsecuritymaster/SessionManagementConfig.java
+++ src/main/java/io/security/springsecuritymaster/SessionManagementConfig.java
@@ -2,39 +2,34 @@ package io.security.springsecuritymaster;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AnonymousAuthenticationProvider;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import java.util.List;
 
 @EnableWebSecurity
 @Configuration
 /**
- * Spring MVC 로그인 구현 Config
+ * 세션 관리 Config
  */
-public class SpringMvcLoginConfig {
-    //@Bean
+public class SessionManagementConfig {
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login").permitAll()
                         .anyRequest().authenticated())
-                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(Customizer.withDefaults())
+                .sessionManagement(session -> session
+                        // 세션 제어 개수 설정해야 세션 제어하는 의미가 있음
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(false) // default: false
+                )
         ;
         return http.build();
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
-    }
 }
