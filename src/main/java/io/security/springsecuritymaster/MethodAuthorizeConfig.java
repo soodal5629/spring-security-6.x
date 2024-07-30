84577f6cf265663af96ca2c9f160ffa645321428
jcl <chaelynjang@lotte.net>
2024-07-30 13:30:38 +0900

    보안 필터 거치지 않아도 되는 정적 자원 관리 확인

--- src/main/java/io/security/springsecuritymaster/MethodAuthorizeConfig.java
+++ src/main/java/io/security/springsecuritymaster/MethodAuthorizeConfig.java
@@ -1,11 +1,13 @@
 package io.security.springsecuritymaster;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,10 +19,21 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class MethodAuthorizeConfig {
+
+    // @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (webSecurity) -> {
+            // atCommonLocations 타고 들어가보면 StaticResourceLocation에 enum으로 정적 자원 경로 지정되어 있움
+            // enum으로 등록된 경로들은 static 폴더 밑에 있어야 함
+            webSecurity.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+        };
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/method/permitAll").permitAll()
+                .requestMatchers("/images/**").permitAll() // ignoring 보다 해당 방식을 권장
+                .requestMatchers("/method/permitAll").permitAll()
                 .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
