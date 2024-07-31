36bfd8d2158014fdadf51ae6c6052cc42e538b65
jcl <chaelynjang@lotte.net>
2024-07-31 11:32:30 +0900

    Authorization에서 커스텀 접두사 설정 및 적용

--- src/main/java/io/security/springsecuritymaster/CustomRolePrefixConfig.java
+++ src/main/java/io/security/springsecuritymaster/CustomRolePrefixConfig.java
@@ -1,15 +1,12 @@
 package io.security.springsecuritymaster;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,36 +15,31 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class RoleHierarchyConfig {
-
-    //@Bean
+public class CustomRolePrefixConfig {
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/user").hasRole("USER")
-                .requestMatchers("/db").hasRole("DB")
-                .requestMatchers("/admin").hasRole("ADMIN")
-                .anyRequest().authenticated())
+                        .requestMatchers("/user").hasRole("USER")
+                        .requestMatchers("/db").hasRole("DB")
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
         ;
         return http.build();
     }
 
-    //@Bean
-    public RoleHierarchy roleHierarchy() {
-        // setHierarchy deprecated 됨
-        // 권한 계층 설정
-        return new RoleHierarchyImpl().fromHierarchy("ROLE_ADMIN > ROLE_DB\n" +
-            "ROLE_DB > ROLE_USER\n" +
-            "ROLE_USER > ROLE_ANONYMOUS");
+    @Bean
+    public GrantedAuthorityDefaults grantedAuthorityDefaults() {
+        // 권한의 접두어를 커스텀하게 변경
+        return new GrantedAuthorityDefaults("MYPREFIX_");
     }
 
-    //@Bean
+    @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
+        UserDetails user = User.withUsername("user").password("{noop}1111").authorities("MYPREFIX_USER").build();
         UserDetails db = User.withUsername("db").password("{noop}1111").roles("DB").build();
         UserDetails admin = User.withUsername("admin").password("{noop}1111").roles("ADMIN", "SECURE").build();
         return new InMemoryUserDetailsManager(user, db, admin);
     }
-
 }
